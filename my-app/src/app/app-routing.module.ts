6302ea1a7aae42b8701da7141dc89480300e4e9b
pm733910 <prajakta.mahamuni@broadcom.com>
2022-12-19 16:15:12 +0530

    modification

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ObservablesComponent} from './observables/observables.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'observables', component: ObservablesComponent},
+  { path: 'dialog', component: MyDialogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservablesComponent,
+    MyDialogComponent
   ],
   imports: [
     BrowserModule,
